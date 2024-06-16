dff685887f18219ad4bd0fae27d0ac51475e5d65
Todor Popov <todorapopov@outlook.com>
2024-06-16 17:48:45 +0300

    testtest

--- ConsoleApp12/Program.cs
+++ ConsoleApp12/Program.cs
@@ -1,7 +1,14 @@
 ﻿int sum = 0;
-for (int i = 0; i <= 10; i++)
+
+int n = int.Parse(Console.ReadLine());
+
+
+Console.Write("The first {0} natural number is :\n", n);
+for (int i = 0; i <= n; i++)
 {
+    Console.Write(i + " ");
+
     sum += i;
 }
 
-Console.WriteLine(sum);
+Console.WriteLine("The Sum of Natural Number up to {0} terms: {1}", n, sum);
